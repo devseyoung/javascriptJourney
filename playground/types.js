f98e91344b1d373f3bc0f85b2e068379110bac2c
devseyoung <devseyoung@gmail.com>
2019-12-05 00:22:31 +1000

    first day

---
+++ playground/types.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+const name = 'wes'; 
+const middle = "topher";
+const last = `bos`;
+
+// // foward slash
+// const setence = 'she\'s so cool\\';
+
+// // back ticks
+// const setence2 = `she's so "cool"`;
+
+// const hello = "Hello my name is " + name + ". Nice to meet you.";
+
+const hello = `hello my name is ${name}. Nice to meet you. I am ${1 + 100} years old `;
