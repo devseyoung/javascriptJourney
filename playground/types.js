7e5c6ca2c2e5e786d26d3f1d9226e96333f493a7
devseyoung <devseyoung@gmail.com>
2019-12-05 11:43:02 +1000

    studying rest of data types, objects, null and undefined, and booleans and equality

--- playground/types.js
+++ playground/types.js
@@ -13,3 +13,37 @@ const last = `bos`;
 // const hello = "Hello my name is " + name + ". Nice to meet you.";
 
 const hello = `hello my name is ${name}. Nice to meet you. I am ${1 + 100} years old `;
+const age = 100.5;  // type of int
+
+const a = 10;
+const b = 20;
+
+// Math.round()
+// Math.floor()
+// Math.random()
+
+const smarties = 20;
+const kids = 3; 
+const eachKidGets = Math.floor(smarties / kids);
+
+console.log(`Each kids get ${eachKidGets}`);
+
+// Object => properties
+// person.age => 100 (the easiest way to get properties in objects)
+const person = {
+    first : 'wes',
+    last : 'bos',
+    age : 100
+};
+
+// undefined
+let dog;
+console.log(dog);
+
+// booleans => 'if' statements
+// == (bad practice because it is not checking types of data) , === (good one)
+
+let isDrawing = false;
+const age = 18;
+const ofAge = age > 10;
+console.log(ofAge);
