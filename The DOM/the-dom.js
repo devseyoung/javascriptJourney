f30e145d6f3c0698569fa464d6333ba494c9e8a7
devseyoung <devseyoung@gmail.com>
2019-12-13 11:54:37 +1000

    custom data attributes

--- The DOM/the-dom.js
+++ The DOM/the-dom.js
@@ -40,3 +40,18 @@ function toggleRound() {
         pic.classList.toggle('round');
 }
 pic.addEventListener('click', toggleRound);
+
+pic.alt = 'Cute Pup'; // setter
+console.log(pic.alt); // getter
+console.log(pic.naturalWidth); // getter
+
+// console.log(pic.getAttribute('alt'));
+
+// data attribute
+const custom = document.querySelector('.custom');
+console.log(custom.dataset);
+
+custom.addEventListener('click', function() {
+        alert(`Welcome ${custom.dataset.name} ${custom.dataset.last}`);
+});
+
