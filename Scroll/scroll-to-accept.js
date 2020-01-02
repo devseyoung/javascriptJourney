623c2ff21cc1d4ed5d3ba41f421576c1d61ab3ea
devseyoung <devseyoung@gmail.com>
2020-01-02 16:13:04 +1000

    edit eslint file

--- Scroll/scroll-to-accept.js
+++ Scroll/scroll-to-accept.js
@@ -2,11 +2,11 @@ const terms = document.querySelector('.terms-and-conditions');
 const watch = document.querySelector('.watch');
 const button = document.querySelector('.accept');
 function obCallback(payload) {
-        if (payload[0].intersectionRatio === 1) {
-                button.disabled = false;
-                // stop observing the button
-                ob.unobserve(terms.lastElementChild);
-        }
+   if (payload[0].intersectionRatio === 1) {
+      button.disabled = false;
+      // stop observing the button
+      ob.unobserve(terms.lastElementChild);
+   }
 }
 
 // console.log(terms);
@@ -16,8 +16,8 @@ function obCallback(payload) {
 // });
 
 const ob = new IntersectionObserver(obCallback, {
-        root: terms,
-        threshold: 1,
+   root: terms,
+   threshold: 1,
 });
 
 // ob.observe(watch);
